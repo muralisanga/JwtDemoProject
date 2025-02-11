cff193b15d2b6d9e510aedeecf16c1493aea0633
muralisanga <58967837+muralisanga@users.noreply.github.com>
2025-02-11 13:11:16 +0530

    Add files via upload

---
+++ JWTDemoProject/src/main/java/com/example/demo/JwtDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtDemoProjectApplication.class, args);
+	}
+
+}
